d0c281cb4a3054bdeb0991037a90284601f2cfa8
DIOGO CARVALHO SOUZA <00001075997070sp@aluno.educacao.sp.gov.br>
2024-11-22 14:29:44 -0300

    alterei a imagem de fundo

--- javascript/criarconta.js
+++ javascript/criarconta.js
@@ -2,16 +2,13 @@ document
   .getElementById("createAccountForm")
   .addEventListener("submit", function (event) {
     event.preventDefault();
-    window.location.href = "index.html";
-  });
-document
-  .getElementById("createAccountForm")
-  .addEventListener("submit", function (event) {
+
     var password = document.getElementById("password").value;
     var confirmPassword = document.getElementById("confirmar-senha").value;
 
     if (password !== confirmPassword) {
-      event.preventDefault();
       alert("Passwords do not match! Please, try again.");
+    } else {
+      window.location.href = "/index.html";
     }
   });
